bb4bd267303fbea66e4c33c4e2c5c80481fec345
bshap93 <benihana858@gmail.com>
2016-07-02 15:22:15 -0500

    initial

---
+++ app.rb
@@ -0,0 +1,42 @@
+require 'rubygems'
+require 'bundler'
+require "sinatra/reloader"
+
+Bundler.require
+
+Dir.glob('./lib/*.rb') do |model|
+  require model
+end
+
+module Citibike
+	class App < Sinatra::Application
+    configure :development do
+      register Sinatra::Reloader
+    end
+
+    before do
+      json = File.open("data/citibikenyc.json").read
+      @data = MultiJson.load(json)
+    end
+
+    get '/' do
+      erb :home
+    end
+
+    get '/form' do
+      erb :form
+    end
+    
+    post '/form' do
+      "You chose #{params["start"]} and #{params["end"]}"
+    end
+
+    post '/map' do
+      puts params
+      @start = params[:start]
+      @end = params[:end]
+      erb :map
+    end
+
+  end
+end
